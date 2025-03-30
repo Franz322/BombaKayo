af6f362127234f528aa2b891a37976040d65ba5b
Franz322 <fpmanrique@astean.biscast.edu.ph>
2025-03-30 22:29:27 +0800

    jump

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,24 +5,82 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    Vector2 movementInput;
-    Rigidbody rb;
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
 
-    private void Start()
+    #region Variables: Movement
+    [SerializeField] private float speed;
+    #endregion
+
+    #region Variables: Rotation
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    #endregion
+
+    #region Variables: Gravity
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+    #endregion
+
+    #region Variables: Jumping
+    [SerializeField] private float jumpPower;
+    #endregion
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 movement = new Vector3(movementInput.x, 0f , movementInput.y);
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void ApplyGravity()
     {
-        movementInput = context.ReadValue<Vector2>();
+        if (IsGrounded() && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        }
+
+        _direction.y = _velocity;
     }
 
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded()) return;
+
+        _velocity += jumpPower;
+    }
+
+    private bool IsGrounded() => _characterController.isGrounded;
 }
